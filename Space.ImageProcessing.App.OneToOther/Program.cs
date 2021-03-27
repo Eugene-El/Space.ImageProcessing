11e8f395e8098107b977b81207bc35a1f4c162d2
El <lomashonok@inbox.lv>
2021-03-27 23:13:58 +0200

    Mirror image population
    - Added mirror populator

--- Space.ImageProcessing.App.OneToOther/Program.cs
+++ Space.ImageProcessing.App.OneToOther/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Space.ImageProcessing.Lib.ImageLoaders;
 using Space.ImageProcessing.Lib.ImagePopulators;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Space.ImageProcessing.App.OneToOther
 {
@@ -16,12 +19,22 @@ namespace Space.ImageProcessing.App.OneToOther
 
                 BitmapLoader bitmapLoader = new BitmapLoader();
                 RotationPopulator rotationPopulator = new RotationPopulator();
+                MirrorPopulator mirrorPopulator = new MirrorPopulator();
 
                 Console.WriteLine("Loading image");
                 var bitmap = bitmapLoader.LoadImage("Images/heic0910e.jpg");
 
                 Console.WriteLine("Rotating images");
                 var list = rotationPopulator.Populate(bitmap);
+                list.Add(bitmap);
+
+                var mirroredImages = new List<Bitmap>();
+                Console.WriteLine("Mirroring images");
+                Parallel.ForEach(list, image =>
+                {
+                    mirroredImages.Add(mirrorPopulator.Populate(image));
+                });
+                list.AddRange(mirroredImages);
 
                 Console.WriteLine("Saving images in PNG");
                 Directory.CreateDirectory("GeneratedContent");
---
+++ Space.ImageProcessing.Lib/ImagePopulators/MirrorPopulator.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+
+namespace Space.ImageProcessing.Lib.ImagePopulators
+{
+    public class MirrorPopulator
+    {
+        public Bitmap Populate(Bitmap sourceBitmap)
+        {
+            unsafe
+            {
+                BitmapData bitmapData = sourceBitmap.LockBits(new Rectangle(0, 0, sourceBitmap.Width,
+                    sourceBitmap.Height), ImageLockMode.ReadOnly, sourceBitmap.PixelFormat);
+
+                int widthInPixels = bitmapData.Width;
+                int heightInPixels = bitmapData.Height;
+
+                int bytesPerPixel = Image.GetPixelFormatSize(sourceBitmap.PixelFormat) / 8;
+
+                int widthInBytes = widthInPixels * bytesPerPixel;
+                int heightInBytes = heightInPixels * bytesPerPixel;
+
+                Bitmap outputBitmap = new Bitmap(widthInPixels, heightInPixels, bitmapData.PixelFormat);
+                BitmapData outputBitmapData = outputBitmap.LockBits(new Rectangle(0, 0, outputBitmap.Width, outputBitmap.Height), ImageLockMode.WriteOnly, sourceBitmap.PixelFormat);
+
+                byte* sourcePointer = (byte*)bitmapData.Scan0;
+                byte* outputPointer = (byte*)outputBitmapData.Scan0;
+                Parallel.For(0, heightInPixels, y =>
+                {
+                    byte* sourceLine = sourcePointer + (y * bitmapData.Stride);
+                    byte* outputLine = outputPointer + (y * outputBitmapData.Stride);
+
+                    for (int x = 0; x < widthInPixels; x++)
+                    {
+                        var currentPosition = x * bytesPerPixel;
+                        var revercedPosition = widthInBytes - bytesPerPixel - x * bytesPerPixel;
+
+                        outputLine[revercedPosition] = sourceLine[currentPosition];
+                        outputLine[revercedPosition + 1] = sourceLine[currentPosition + 1];
+                        outputLine[revercedPosition + 2] = sourceLine[currentPosition + 2];
+                    }
+                });
+                sourceBitmap.UnlockBits(bitmapData);
+                outputBitmap.UnlockBits(outputBitmapData);
+
+                return outputBitmap;
+            }
+        }
+    }
+}
